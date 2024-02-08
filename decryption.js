7c1825074c2dfd035f72126f0dc1c8b15a0e5803
KarungiRMS <KarungiRMSonia@gmail.com>
2024-02-08 06:08:20 +0300

    Encryption, Decryption and styling

---
+++ decryption.js
@@ -0,0 +1,50 @@
+const decryptForm = document.getElementById("decryptForm");
+const textToDecrypt = document.getElementById("textToDecrypt");
+const decryptDiv = document.getElementById('decryptedResults');
+
+//Decrypt each character
+function decryptLetter(letter, shift){
+  if (!alphabet.includes(letter)){
+      return letter;
+  }
+  const idx = alphabet.indexOf(letter.toLowerCase());
+  let newIdx = (idx - shift + alphabet.length) % alphabet.length;
+  if (newIdx < 0) newIdx = (newIdx + alphabet.length) % alphabet.length;
+  return alphabet[newIdx];
+
+}
+
+//Decrypt entire message 
+function decrypt (encryptedNewMessage, shift)
+{
+  let decryptedWord = "";
+  for (let letter of encryptedNewMessage){
+      decryptedWord += decryptLetter(letter,shift);
+  }
+  let decryptedNewMessage = "";
+  let counter = 0;
+  for (let i = 0; i <decryptedWord.length; i++){
+      if (counter == 2){
+          counter =0;
+      }
+      else{
+          decryptedNewMessage += decryptedWord[i];
+          counter++;
+      }
+  }
+  console.log(decryptedNewMessage);
+  return decryptedNewMessage;
+}
+
+
+decryptForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const encryptedMessage = decrypt(textToDecrypt.value, 42);
+  const plainText = document.createElement('p');
+  const decryptResults = document.createElement('p');
+  plainText.innerText = `Original(Encrypted Text) text is: ${textToDecrypt.value}`;
+  decryptResults.innerText = `Decrypted text is: ${encryptedMessage}`;
+  decryptDiv.appendChild(plainText);
+  decryptDiv.appendChild(decryptResults);
+  decryptForm.reset();
+});
---
+++ encryption.js
@@ -0,0 +1,50 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const encryptForm = document.getElementById("encryptForm");
+const textToEncrypt = document.getElementById("textToEncrypt");
+const encryptDiv = document.getElementById('encryptedResults');
+
+//To encrypt each letter
+function encryptLetter(letter, shift) {
+  if (!alphabet.includes(letter)) {
+    return letter;
+  }
+  const idx = alphabet.indexOf(letter.toLowerCase());
+  const newIdx = (idx + shift) % alphabet.length;
+  return alphabet[newIdx];
+}
+
+//Random character generator
+function randomChar() {
+  let randomIdx = Math.floor(Math.random() * alphabet.length);
+  let randomChar = alphabet[randomIdx];
+  return randomChar;
+}
+
+//Encrypt entire message
+function encrypt(message, shift) {
+  let encryptedMessage = "";
+  for (let letter of message) {
+    encryptedMessage += encryptLetter(letter, shift);
+  }
+  let encryptedNewMessage = "";
+  for (let i = 0; i < encryptedMessage.length; i++) {
+    if (i % 2 === 0 && i !== 0) {
+      encryptedNewMessage += randomChar() + encryptedMessage[i];
+    } else {
+      encryptedNewMessage += encryptedMessage[i];
+    }
+  }
+  return encryptedNewMessage;
+}
+
+encryptForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const encryptedMessage = encrypt(textToEncrypt.value, 42);
+  const plainText = document.createElement('p');
+  const encryptResults = document.createElement('p');
+  plainText.innerText = `Original text is: ${textToEncrypt.value}`;
+  encryptResults.innerText = `Encrypted text is: ${encryptedMessage}`;
+  encryptDiv.appendChild(plainText);
+  encryptDiv.appendChild(encryptResults);
+  encryptForm.reset();
+});
